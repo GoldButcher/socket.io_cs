95a9dfd372a2f7a5cc031eebb123b1f4a4855b1d
947538570@qq.com <a63728477>
2017-07-24 19:45:59 +0800

    update

---
+++ mysql.js
@@ -0,0 +1,41 @@
+var mysql = require('mysql');
+//填写数据库连接信息，可查询数据库详情页
+var username = 'root';
+var password = '123456';
+var db_host = '127.0.0.1';
+var db_port = 3306;
+var db_name = 'kefu';
+var option = {
+    host: db_host,
+    port: db_port,
+    user: username,
+    password: password,
+    database: db_name
+};
+
+function _exec(sqls,values,after) {
+    var client = mysql.createConnection(option);
+
+
+    client.connect(function(err,result){
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        client.query(sqls || '', values || [],function(err,r){
+            after(err,r);
+        });
+        client.end();
+
+    });
+    client.on('error',function(err) {
+        if (err.errno != 'ECONNRESET') {
+            after("err01",false);
+            throw err;
+        } else {
+            after("err02",false);
+        }
+    });
+}
+exports.exec = _exec;
